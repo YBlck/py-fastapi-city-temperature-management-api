91e9c4919a9414ab869fb6b046d374426114dcab
Yura Chornyi <chorny.yura@gmail.com>
2025-07-01 16:56:57 +0300

    feat: add schemas for City model

---
+++ cities/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class CityBase(BaseModel):
+    name: str
+    additional_info: str | None = None
+
+
+class CityCreate(CityBase):
+    pass
+
+
+class CityResponse(CityBase):
+    id: int
+
+    model_config = {"from_attributes": True}
