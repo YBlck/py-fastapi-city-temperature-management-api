01e7aa97f79d60398445f0a0c14353cbb093e8b0
Yura Chornyi <chorny.yura@gmail.com>
2025-07-02 01:31:21 +0300

    feat: add relationship in City model to Temperature

--- cities/models.py
+++ cities/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
 
@@ -10,6 +10,8 @@ class City(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     additional_info: Mapped[str] = mapped_column(Text, nullable=True)
+
+    temperatures: Mapped[list["Temperature"]] = relationship(back_populates="city")
 
     def __repr__(self):
         return f"<City {self.name}>"
