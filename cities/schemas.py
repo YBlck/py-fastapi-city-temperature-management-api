814c014b7ad8165c207f330d51236798fd114187
Yura Chornyi <chorny.yura@gmail.com>
2025-07-01 20:17:38 +0300

    feat: add schema for City update

--- cities/schemas.py
+++ cities/schemas.py
@@ -1,13 +1,19 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class CityBase(BaseModel):
     name: str
-    additional_info: str | None = None
+    additional_info: Optional[str] = None
 
 
 class CityCreate(CityBase):
     pass
+
+
+class CityUpdate(BaseModel):
+    additional_info: Optional[str] = None
 
 
 class CityResponse(CityBase):
